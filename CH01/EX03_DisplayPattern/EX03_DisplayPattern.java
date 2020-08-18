d2f56aec14ad63435dea6c56f970c54b356fd7de
Chris Hill <chill4820@gmail.com>
2020-08-17 23:20:59 -0400

    first commit

---
+++ CH01/EX03_DisplayPattern/EX03_DisplayPattern.java
@@ -0,0 +1,22 @@
+/**
+ * (Display a pattern) Write a program that displays the following pattern:
+ *
+ * Commentary: I will use println statement and space letters as best I can. 
+ *
+ * Author: chill4Eng2Dev 
+ * Date: 8/17/2020
+ * Book: Introduction to Java Programming, 10th Edition, Y. Daniel Liang
+ * Exercise: 1.3
+ */
+ 
+public class EX03_DisplayPattern {
+ 
+   public static void main(String[] args) {
+   
+      System.out.println("    J    A    V     V    A    ");
+      System.out.println("    J   A A    V   V    A A   ");
+      System.out.println("J   J  AAAAA    V V    AAAAA  ");
+      System.out.println(" J J  A     A    V    A     A ");
+   }
+ 
+}
