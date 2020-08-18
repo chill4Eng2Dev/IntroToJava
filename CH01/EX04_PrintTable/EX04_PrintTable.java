bfcc86705bb331d104fc4a9198f4d01ec7c5a92e
Chris Hill <chill4820@gmail.com>
2020-08-17 23:33:32 -0400

    first commit

---
+++ CH01/EX04_PrintTable/EX04_PrintTable.java
@@ -0,0 +1,29 @@
+/**
+ * (Print a table) Write a program that displays the following table:
+ *
+ * a     a^2     a^3
+ * 1     1       1
+ * 2     4       8
+ * 3     9       27
+ * 4     16      64
+ *
+ * Commentary: I will use println statement and space letters as best I can. 
+ *
+ * Author: chill4Eng2Dev 
+ * Date: 8/17/2020
+ * Book: Introduction to Java Programming, 10th Edition, Y. Daniel Liang
+ * Exercise: 1.4
+ */
+ 
+public class EX04_PrintTable {
+ 
+   public static void main(String[] args) {
+   
+      System.out.println("a     a^2   a^3");
+      System.out.println("1     1     1  ");
+      System.out.println("2     4     8  ");
+      System.out.println("3     9     27 ");
+      System.out.println("4     16    64 ");
+   
+   }
+}
