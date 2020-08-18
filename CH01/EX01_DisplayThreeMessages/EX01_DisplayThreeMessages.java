4a99a6b41348a49d2a655b022ff31afaf79d1476
Chris Hill <chill4820@gmail.com>
2020-08-17 23:13:33 -0400

    first commit

---
+++ CH01/EX01_DisplayThreeMessages/EX01_DisplayThreeMessages.java
@@ -0,0 +1,24 @@
+/**
+ * (Display three message) Write a program that displays Welcome to Java,
+ * Welcome to Computer Science, and Programming is fun.
+ *
+ * Commentary: I will use three println statements and print out the
+ * statements.
+ *
+ * Author: chill4Eng2Dev 
+ * Date: 8/17/2020
+ * Book: Introduction to Java Programming, 10th Edition, Y. Daniel Liang
+ * Exercise: 1.1
+ */
+ 
+public class EX01_DisplayThreeMessages {
+ 
+   public static void main(String[] args) {
+   
+      System.out.println("Welcome to Java");
+      System.out.println("Welcome to Computer Science");
+      System.out.println("Programming is fun");
+   
+   }
+ 
+}
---
+++ CH01/EX02_DisplayFiveMessages/EX02_DisplayFiveMessages.java
@@ -0,0 +1,24 @@
+/**
+ * (Display five messages) Write a program that displays Welcome to Java five times.
+ *
+ * Commentary: I will use 5 println statements 
+ *
+ * Author: chill4Eng2Dev 
+ * Date: 8/17/2020
+ * Book: Introduction to Java Programming, 10th Edition, Y. Daniel Liang
+ * Exercise: 1.2
+ */
+ 
+public class EX02_DisplayFiveMessages {
+ 
+   public static void main(String[] args) {
+   
+      System.out.println("Welcome to Java");
+      System.out.println("Welcome to Java");
+      System.out.println("Welcome to Java");
+      System.out.println("Welcome to Java");
+      System.out.println("Welcome to Java");
+         
+   }
+ 
+}
